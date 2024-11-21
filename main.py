7b1cb43b22b1b92fab4d357eea8e426c9a04c07e
MakCimka <poduskapuska@gmail.com>
2024-11-21 10:26:42 +0300

    main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 stroka1 ="яблоко"
 stroka2 ="яблоко"
-n = stroka1.replace(" ", "")
+n = stroka1.replace(" ", "") #Убираем пробелы
 h = len(n)
-y = stroka2.replace(" ", "")
+y = stroka2.replace(" ", "") #Убираем пробелы
 count = 0
-for i in n:
-    if i in y:
-        count +=1
-if count == h and h==len(y):
+for i in n: #Проверяем символы
+    if i in y: #Проверяем символы
+        count +=1 #Увеличивааем счетчик
+if count == h and h==len(y): #Сравниваем длину строчек
         print("Анаграмма")
 else:
         print("Не анаграмма" )
